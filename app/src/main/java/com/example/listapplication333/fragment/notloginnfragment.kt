f07b1a542dfd13bafc0b505077eb5b627b920126
hyerin202010854 <yhkh0808@naver.com>
2022-08-17 15:15:54 +0900

    로그인버튼(로그인기능은x) 까지 구현222 구글 로그인 했는지 안했는지 분기에 따라 로그인 버튼 화면 이동 구현 완료 이후 로그인 기능 하다가 망한 코드

--- app/src/main/java/com/example/listapplication333/fragment/notloginnfragment.kt
+++ app/src/main/java/com/example/listapplication333/fragment/notloginnfragment.kt
@@ -1,6 +1,8 @@
 package com.example.listapplication333.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,11 @@ import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.listapplication333.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_mainfragment.*
 import kotlinx.android.synthetic.main.fragment_notloginnfragment.*
 
@@ -22,6 +29,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class notloginnfragment : Fragment() {
+
+    //firebase Auth
+    private lateinit var firebaseAuth: FirebaseAuth
+    //google client
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    //private const val TAG = "GoogleActivity"
+    private val RC_SIGN_IN = 99
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -53,7 +69,42 @@ class notloginnfragment : Fragment() {
             navController.navigate(R.id.action_notloginnfragment_to_mainfragment)
         }
 
+        //btn_googleSignIn.setOnClickListener (this) // 구글 로그인 버튼
+        btn_googleSignIn.setOnClickListener {this}
+
+        //Google 로그인 옵션 구성. requestIdToken 및 Email 요청
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            //'R.string.default_web_client_id' 에는 본인의 클라이언트 아이디를 넣어주시면 됩니다.
+            //저는 스트링을 따로 빼서 저렇게 사용했지만 스트링을 통째로 넣으셔도 됩니다.
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        //firebase auth 객체
+        firebaseAuth = FirebaseAuth.getInstance()
+
     }
+
+    // onActivityResult
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("LoginActivity", "Google sign in failed", e)
+            }
+        }
+    } // onActivityResult End
 
 
 
